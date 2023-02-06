d6bbb6126910c2f44699cb3c3460f0429fdd8bcf
saff84 <saff84@mail.ru>
2023-02-06 18:09:24 +0300

    task 3 done

---
+++ Выпадающий список с поиском/task.js
@@ -0,0 +1,105 @@
+class Autocomplete {
+    constructor( container ) {
+      this.container = container;
+      this.input = container.querySelector( '.autocomplete__input' );
+      this.searchInput = container.querySelector( '.autocomplete__search' );
+      this.list = container.querySelector( '.autocomplete__list' );
+      this.valueContainer = container.querySelector( '.autocomplete__value' );
+      this.valueElement = container.querySelector( '.autocomplete__text-content' );
+  
+      this.registerEvents();
+    }
+  
+    registerEvents() {
+      this.valueContainer.addEventListener( 'click', e => {
+        this.searchInput.classList.add( 'autocomplete__search_active' );
+        this.list.classList.add( 'autocomplete__list_active' );
+        this.searchInput.value = this.valueElement.textContent.trim();
+        this.searchInput.focus();
+  
+        this.onSearch();
+      });
+  
+  
+      this.searchInput.addEventListener( 'input', e => this.onSearch());
+  
+      this.list.addEventListener( 'click', e => {
+        const { target } = e;
+        if ( !target.matches( '.autocomplete__item' )) {
+          return;
+        }
+  
+        const { textContent: text } = target,
+          { id: value, index } = target.dataset;
+  
+        this.onSelect({
+          index,
+          text,
+          value
+        });
+      });
+    }
+  
+    onSelect( item ) {
+      this.input.selectedIndex = item.index;
+      this.valueElement.textContent = item.text;
+  
+      this.searchInput.classList.remove( 'autocomplete__search_active' );
+      this.list.classList.remove( 'autocomplete__list_active' );
+    }
+  
+    onSearch() {
+      const matches = this.getMatches( this.searchInput.value );
+  
+      this.renderMatches( matches );
+    }
+  
+    renderMatches( matches ) {
+      const html = matches.map( item => `
+          <li>
+          <span class="autocomplete__item"
+              data-index="${item.index}"
+            data-id="${item.value}"
+          >${item.text}</span>
+        </li>
+      `);
+  
+      this.list.innerHTML = html.join('');
+    }
+  
+    getMatches( text ) {
+        let options = this.input.options;
+        let arrSelect = [];
+          for (let i = 0; i < options.length; i++){
+    
+            if (options[i].text.includes(text)) {
+              arrSelect.push({ text: options[i].text, value: options[i].value })
+            }
+            
+          }
+    
+          return arrSelect;
+      }
+         
+        /*
+        TODO: этот метод нужно дописать
+        text - фраза, которую вводят в поле поиска
+        Метод должен вернуть массив.
+        Он формируется на основе списка опций select-элемента (this.input)
+        Подходящие опции - те, чей текст содержит то, что есть в аргументе text
+        Необходимо вернуть массив объектов со свойствами:
+        {
+          text: 'Содержимое <option>',
+          value: 'Содержимое атрибута value'
+        }
+      */
+    //   return [
+    //     {
+    //       text: 'Чубакка',
+    //       value: '1'
+    //     }
+    //   ];
+    // }
+  }
+  
+  new Autocomplete( document.querySelector( '.autocomplete' ));
